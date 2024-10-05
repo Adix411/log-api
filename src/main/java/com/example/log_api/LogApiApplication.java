8513a3aaa1abe3e7f6a36fc2de7bc33ced67f3b1
Adix411 <adix411@wp.pl>
2024-10-05 12:15:25 +0200

    commit

---
+++ src/main/java/com/example/log_api/LogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.log_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogApiApplication.class, args);
+	}
+
+}
